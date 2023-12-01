8afd59c4f5cbb29b1cd52b5a281f590d0aed64c3
rizkida <rizkyananda123439@gmail.com>
2023-12-01 09:20:11 +0700

    entity checkin checkout receptionist

--- src/main/java/com/jdt13/hotel/entity/Receptionist.java
+++ src/main/java/com/jdt13/hotel/entity/Receptionist.java
@@ -1,4 +1,21 @@
 package com.jdt13.hotel.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Receptionist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nama;
+    private String username;
+    private String password;
+    private String token;
+
 }
