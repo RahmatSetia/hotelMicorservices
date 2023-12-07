4d72e856801f174b893ee1a7175fcbe6a7bd8cdb
rahmat <rahmatsetiq@gmail.com>
2023-12-07 09:08:09 +0700

    kamar srvice, kamar repository, kamar dto

--- src/main/java/com/jdt13/hotel/repository/KamarRepository.java
+++ src/main/java/com/jdt13/hotel/repository/KamarRepository.java
@@ -2,11 +2,22 @@ package com.jdt13.hotel.repository;
 
 import com.jdt13.hotel.entity.Kamar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface KamarRepository extends JpaRepository<Kamar, Integer> {
     Optional<Kamar> findByNoKamar(Integer noKamar);
+    @Query("SELECT k.id, k.noKamar, k.harga " +
+            "FROM Kamar k " +
+            "LEFT JOIN Booking b ON k.id = b.kamar.id " +
+            "AND :checkin > b.checkin " +
+            "AND :checkout < b.checkout " +
+            "WHERE b.statusBooking IS NULL")
+    List<Kamar> findKamarBeforeBookingInCheckinCheckout(@Param("checkin") Date checkin, @Param("checkout") Date checkout);
 }
--- src/main/java/com/jdt13/hotel/service/KamarService.java
+++ src/main/java/com/jdt13/hotel/service/KamarService.java
@@ -3,11 +3,14 @@ package com.jdt13.hotel.service;
 import com.jdt13.hotel.dto.KamarRequest;
 import com.jdt13.hotel.dto.KamarResponse;
 import com.jdt13.hotel.entity.Kamar;
+import com.jdt13.hotel.exception.ApiRequestException;
 import com.jdt13.hotel.repository.KamarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,8 +19,11 @@ public class KamarService {
 
     private final KamarRepository kamarRepository;
 
-    public KamarResponse saveKamar(KamarRequest request){
+    public List<Kamar> getAllKamarBeforeBooking (Date dateIn, Date dateOut){
+        return kamarRepository.findKamarBeforeBookingInCheckinCheckout(dateIn, dateOut);
+    }
 
+    public KamarResponse saveKamar(KamarRequest request){
         Kamar kamar = new Kamar();
         kamar.setNoKamar(request.getNoKamar());
         kamar.setHarga(BigDecimal.valueOf(request.getHarga()));
@@ -33,26 +39,34 @@ public class KamarService {
         return response;
     }
 
-    public KamarResponse updateKamarIdKamar(KamarRequest request){
-
-        Optional<Kamar> kamar = kamarRepository.findByNoKamar(request.getNoKamar());
-
-        if (kamar.isEmpty()){
-            throw new IllegalArgumentException("Kamar tidak ditemukan");
+    public KamarResponse updateKamarById (Integer id, KamarRequest request){
+        String pesan = "Kamar tidak di temukan";
+        Optional<Kamar> k = kamarRepository.findById(id);
+        if (k.isEmpty()){
+            throw new ApiRequestException(pesan);
         }
-            KamarResponse response = new KamarResponse();
-            response.setNoKamar(request.getNoKamar());
-            return response;
+        Kamar kamar = new Kamar();
+        kamar.setId(k.get().getId());
+        kamar.setNoKamar(request.getNoKamar());
+        kamar.setHarga(BigDecimal.valueOf(request.getHarga()));
+        kamar.setKategori(request.getKategori());
+        kamar.setDeskripsi(request.getDeskripsi());
+        kamarRepository.save(kamar);
+
+        KamarResponse response = new KamarResponse();
+        response.setId(kamar.getId());
+        response.setNoKamar(request.getNoKamar());
+        response.setHarga(request.getHarga());
+        response.setKategori(request.getKategori());
+        response.setDeskripsi(request.getDeskripsi());
+        return response;
     }
 
     public String deleteKamarIdKamar(Integer id){
-
         Optional<Kamar> kamarId = kamarRepository.findById(id);
-
-        String pesan = "";
-
+        String pesan = "Id kamar tidak di temukan";
         if (kamarId.isEmpty()){
-            throw new IllegalArgumentException("Kamar sudah di booking");
+            throw new ApiRequestException(pesan);
         }
         kamarRepository.deleteById(id);
         return pesan;
