6ca86ef24bddf466fdd6b6c17bb56b6dc3ef4bba
Rahmat Fadilah <rahmat.fadilah02@gmail.com>
2023-12-19 23:02:49 +0700

    Unit testing customer

--- src/test/java/com/jdt13/hotel/controller/CustomerControllerTest.java
+++ src/test/java/com/jdt13/hotel/controller/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package com.jdt13.hotel.controller;
 
+import com.jdt13.hotel.dto.CustomerRequest;
 import com.jdt13.hotel.dto.CustomerResponse;
 import com.jdt13.hotel.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,82 +34,53 @@ class CustomerControllerTest {
         // Arrange
         String token = "token 1";
         int customerId = 1;
-        CustomerResponse mockCustomer = new CustomerResponse();
-        when(customerService.findCustomerById(customerId, token)).thenReturn(mockCustomer);
+        CustomerResponse response = new CustomerResponse();
+        response.setId(customerId);
+        response.setNama("fulan");
+        response.setPassword("test123");
+        response.setUsername("user");
+        response.setPhone("085846564");
+        response.setAlamat("jl sedayu group");
+
+
+        when(customerService.findCustomerById(customerId, token)).thenReturn(response);
 
         // Act
         ResponseEntity<CustomerResponse> responseEntity = customerController.getCustomerById(token, customerId);
 
         // Assert
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(mockCustomer, responseEntity.getBody());
-        verify(customerService, times(1)).findCustomerById(customerId);
-    }
-
-    void testGetCustomerById(){
-        CustomerResponse response = new CustomerResponse();
-        when(customerService.findCustomerById()).thenReturn();
-        ResponseEntity<>
+        assertEquals(response, responseEntity.getBody());
+        verify(customerService, times(1)).findCustomerById(customerId, token);
     }
 
     @Test
     void testRegisterCustomer() {
         // Arrange
-        CustomerRequest customerRequest = new CustomerRequest();
-        CustomerResponse mockResponse = new CustomerResponse();
-        when(customerService.registerCustomer(customerRequest)).thenReturn(mockResponse);
+        Integer id = 1;
+        CustomerRequest request = new CustomerRequest();
+        request.setNama("Fulan");
+        request.setPassword("test123");
+        request.setUsername("user2");
+        request.setPhone("045445454");
+        request.setAlamat("jl jl indivara");
+
+        CustomerResponse response = new CustomerResponse();
+        response.setId(id);
+        response.setNama(request.getNama());
+        response.setPassword(request.getPassword());
+        response.setUsername(request.getUsername());
+        response.setPhone(request.getPhone());
+        response.setAlamat(request.getAlamat());
+
+        when(customerService.registerCustomer(request)).thenReturn(response);
 
         // Act
-        ResponseEntity<CustomerResponse> responseEntity = customerController.registerCustomer(customerRequest);
+        ResponseEntity<CustomerResponse> responseEntity = customerController.registerCustomer(request);
 
         // Assert
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals(mockResponse, responseEntity.getBody());
-        verify(customerService, times(1)).registerCustomer(customerRequest);
-    }
-
-    @Test
-    void testLogin() {
-        // Arrange
-        LoginRequest loginRequest = new LoginRequest();
-        LoginResponse mockResponse = new LoginResponse();
-        when(customerService.login(loginRequest)).thenReturn(mockResponse);
-
-        // Act
-        ResponseEntity<LoginResponse> responseEntity = customerController.login(loginRequest);
-
-        // Assert
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(mockResponse, responseEntity.getBody());
-        verify(customerService, times(1)).login(loginRequest);
-    }
-
-    @Test
-    void testUpdateCustomerById() {
-        // Arrange
-        int customerId = 1;
-        CustomerRequest customerRequest = new CustomerRequest();
-        CustomerResponse mockResponse = new CustomerResponse();
-        when(customerService.updateCustomer(customerId, customerRequest)).thenReturn(mockResponse);
-
-        // Act
-        ResponseEntity<CustomerResponse> responseEntity = customerController.updateCustomerById(customerId, customerRequest);
-
-        // Assert
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(mockResponse, responseEntity.getBody());
-        verify(customerService, times(1)).updateCustomer(customerId, customerRequest);
-    }
-
-    @Test
-    void testDeleteById() {
-        // Arrange
-        int customerId = 1;
-
-        // Act
-        customerController.deleteById(customerId);
-
-        // Assert
-        verify(customerService, times(1)).deleteCustomerById(customerId);
+        assertEquals(response, responseEntity.getBody());
+        verify(customerService, times(1)).registerCustomer(request);
     }
 }
